e7d5533af573780aeb95253cefc1847f3d1fb58e
Lewis <llcawthorne@gmail.com>
2026-08-28 08:56:16 -0400

    Update grip path

--- grip/.grip/settings.py
+++ grip/.grip/settings.py
@@ -1,4 +1,4 @@
 USERNAME = "llcawthorne@gmail.com"
 STYLE_URLS = [
-    "https://raw.githubusercontent.com/llcawthorne/dotfiles/grip/.grip/github-markdown.dark.css"
+    "https://raw.githubusercontent.com/llcawthorne/dotfiles/main/grip/.grip/github-markdown-dark.css"
 ]
